7d7d437f4c9dbc559ca0cdeae043b458f67583e7
mc_dev <mcbootcampdev@gmail.com>
2019-12-11 16:42:36 +0200

    first test of the pipeline no pytest

--- jenkins_files/e2e_test.sh
+++ jenkins_files/e2e_test.sh
@@ -1,41 +1,20 @@
 #/bin/bash
-a=$(docker ps -a | grep "ted_search" | cut -d " " -f9)
+a=$(docker ps -a | grep "myserver" | cut -d " " -f9)
 if [ $a ] 
 then
-    docker rm ted_search -f
+    docker rm myserver -f
     echo "------------------------- docker ted_search removed ---------------------"
 fi
 
-docker run -d -p 9191:9191 --name ted_search ted_image
+docker run -d -p 8080:8080 --name myserver ted_image
 
 hostip=$(/sbin/ip route|awk '/default/ { print $3 }')
 
-if [ -d "venv" ]; then
-  rm -rf venv
-  echo "----------------- virtual environment removed ---------------------"
-fi
-
-virtualenv venv
-. ./venv/bin/activate
-pip install requests
-pip install -U  pytest
-/var/jenkins_home/.local/bin/pytest --ip=http://$hostip:9191
-exit_of_pytest=$?
-deactivate
-
-if [ -d "venv" ]; then
-  rm -rf venv
-  echo "----------------- virtual environment removed ---------------------"
-fi
+sleep 5
 
 a=$(docker ps -a | grep "ted_search" | cut -d " " -f9)
 if [ $a ] 
 then
-    docker rm ted_search -f
+    docker rm myserver -f
     echo "------------------------- docker ted_search removed ---------------------"
-fi
-
-if [ ! $exit_of_pytest -eq 0 ] 
-then
-    exit 1
 fi
